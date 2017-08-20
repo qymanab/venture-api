31d4b1dbdfea6ae6605743297b2036d8dcef052a
Zack Tylor <taylor.zackory@gmail.com>
2017-08-19 20:35:29 -0500

    update seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
+Segment.delete_all
+
+30.times do
   Segment.create(name: Faker::Book.title, body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
 end
