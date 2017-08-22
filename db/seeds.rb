be8d0e4d2a24f00b0eeab1f1031c32417da92983
Zack Tylor <taylor.zackory@gmail.com>
2017-08-22 18:42:00 -0500

    update seed to make random seeds around the world. Make segment genres dependant on segment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Segment.delete_all
-Genre.delete_all
-
-# 30.times do
-#   Segment.create(name: Faker::Book.title, body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
-# end
+Segment.destroy_all
+Genre.destroy_all
 
 adventure = Genre.create(name: "adventure")
 fiction = Genre.create(name: "fiction")
@@ -21,13 +17,30 @@ comedy = Genre.create(name: "comedy")
 romance = Genre.create(name: "romance")
 other = Genre.create(name: "other")
 
- adventure.segments << Segment.new(name:'Chair' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876116499999995' , longitude: '-87.6530416' )
-adventure. segments << Segment.new(name:'couch' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8761018' , longitude: '-87.6529906' )
-fiction.segments << Segment.new(name:'Metis Logo' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876445499999996' , longitude: '-87.65302489999999' )
-nonfiction.segments << Segment.new(name: 'Breakout Room', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876382299999996' , longitude: '-87.653041' )
-horror.segments << Segment.new(name:'Yoda Quote', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763582' , longitude: '-87.6530538' )
-comedy.segments << Segment.new(name:'Kelly Desk', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876419999999996' , longitude: '-87.65305359999999' )
-romance.segments << Segment.new(name:'firt row', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763736' , longitude: '-87.6530294' )
-other.segments << Segment.new(name:'storage', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763736' , longitude: '-87.6530361' )
-horror.segments << Segment.new(name:'office', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876161599999996' , longitude: '-87.6530372' )
+title_from = [
+  Faker::HowIMetYourMother.quote,
+  Faker::RickAndMorty.quote,
+  Faker::HeyArnold.quote,
+  Faker::ChuckNorris.fact,
+  Faker::TheFreshPrinceOfBelAir.quote,
+  Faker::StarWars.quote,
+  Faker::Movie.quote,
+  Faker::Book.title
+]
 
+200.times do
+  s = Segment.create(name: title_from.sample, body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: Faker::Address.latitude, longitude: Faker::Address.longitude)
+  s.genres << Genre.all.sample
+end
+
+
+
+# adventure.segments << Segment.new(name:'Chair' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876116499999995' , longitude: '-87.6530416' )
+# adventure. segments << Segment.new(name:'couch' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8761018' , longitude: '-87.6529906' )
+# fiction.segments << Segment.new(name:'Metis Logo' , body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876445499999996' , longitude: '-87.65302489999999' )
+# nonfiction.segments << Segment.new(name: 'Breakout Room', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876382299999996' , longitude: '-87.653041' )
+# horror.segments << Segment.new(name:'Yoda Quote', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763582' , longitude: '-87.6530538' )
+# comedy.segments << Segment.new(name:'Kelly Desk', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876419999999996' , longitude: '-87.65305359999999' )
+# romance.segments << Segment.new(name:'firt row', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763736' , longitude: '-87.6530294' )
+# other.segments << Segment.new(name:'storage', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.8763736' , longitude: '-87.6530361' )
+# horror.segments << Segment.new(name:'office', body: Faker::Lorem.paragraphs(rand(1..5), true), latitude: '41.876161599999996' , longitude: '-87.6530372' )
